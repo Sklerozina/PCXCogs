607523dc1016b310bff7918df6d336786b6851c8
CrYoZ <neko@nekos-home.ru>
2025-02-07 22:52:44 +0300

    update

--- dice/__init__.py
+++ dice/__init__.py
@@ -1,4 +1,5 @@
 """Package for Dice cog."""
+import asyncio
 import json
 from pathlib import Path
 
@@ -10,7 +11,9 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot: Red) -> None:
+async def setup(bot: Red) -> None:
     """Load Dice cog."""
-    cog = Dice(bot)
-    bot.add_cog(cog)
+    if asyncio.iscoroutinefunction(bot.add_cog):
+            await bot.add_cog(Dice(bot))
+    else:
+        bot.add_cog(Dice(bot))
